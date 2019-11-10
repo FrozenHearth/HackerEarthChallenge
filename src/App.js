c0539d7828ddc1e5caa9fbdeaa70102adcca2a89
Vishu <brs@Bs-MacBook-Air.local>
2019-11-10 09:33:42 +0530

    Added logic for updating price-card component when item quantity & price change in items-list component and added some responsiveness and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ShoppingCart from './components/shoppingCart';
+import ItemsList from './components/itemsList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ShoppingCart />
+        <ItemsList />
       </div>
     );
   }
--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,11 +1,19 @@
+import {
+  INCREASE_QUANTITY,
+  DECREASE_QUANTITY,
+  GET_CART_STATE,
+  DELETE_ITEM
+} from '../actions/types';
+
 const initState = {
-  items: [
+  itemsList: [
     {
       id: 9090,
       name: 'Item1',
       price: 200,
       discount: 10,
       type: 'fiction',
+      quantity: 1,
       img_url:
         'https://store.lexisnexis.com.au/__data/media/catalog/thumb//placeholder.jpg'
     },
@@ -15,6 +23,7 @@ const initState = {
       price: 250,
       discount: 15,
       type: 'literature',
+      quantity: 1,
       img_url:
         'https://store.lexisnexis.com.au/__data/media/catalog/thumb//placeholder.jpg'
     },
@@ -24,6 +33,7 @@ const initState = {
       price: 320,
       discount: 5,
       type: 'literature',
+      quantity: 1,
       img_url:
         'https://store.lexisnexis.com.au/__data/media/catalog/thumb//placeholder.jpg'
     },
@@ -33,6 +43,7 @@ const initState = {
       price: 290,
       discount: 0,
       type: 'thriller',
+      quantity: 1,
       img_url:
         'https://store.lexisnexis.com.au/__data/media/catalog/thumb//placeholder.jpg'
     },
@@ -42,6 +53,7 @@ const initState = {
       price: 500,
       discount: 25,
       type: 'thriller',
+      quantity: 1,
       img_url:
         'https://store.lexisnexis.com.au/__data/media/catalog/thumb//placeholder.jpg'
     },
@@ -51,6 +63,7 @@ const initState = {
       price: 150,
       discount: 5,
       type: 'literature',
+      quantity: 1,
       img_url:
         'https://store.lexisnexis.com.au/__data/media/catalog/thumb//placeholder.jpg'
     },
@@ -60,6 +73,7 @@ const initState = {
       price: 700,
       discount: 22,
       type: 'literature',
+      quantity: 1,
       img_url:
         'https://store.lexisnexis.com.au/__data/media/catalog/thumb//placeholder.jpg'
     },
@@ -69,16 +83,68 @@ const initState = {
       price: 350,
       discount: 18,
       type: 'fiction',
+      quantity: 1,
       img_url:
         'https://store.lexisnexis.com.au/__data/media/catalog/thumb//placeholder.jpg'
     }
   ],
-  addedItems: [],
-  total: 0
+  addedItem: [],
+  totalItems: [],
+  totalPrice: []
 };
 
-const cartReducer = (state = initState, action) => {
-  return state;
-};
+export default (state = initState, action) => {
+  switch (action.type) {
+    case INCREASE_QUANTITY:
+      if (action.type === INCREASE_QUANTITY) {
+        state.addedItem = state.itemsList.find(item => item.id === action.id);
+        state.addedItem.quantity++;
+        const updatedPrice =
+          state.addedItem.price * (state.addedItem.quantity - 1);
+        let newTotal = state.totalPrice + updatedPrice;
+        return { ...state, totalPrice: newTotal, addedItem: state.addedItem };
+      }
+    case DECREASE_QUANTITY:
+      if (action.type === DECREASE_QUANTITY) {
+        state.addedItem = state.itemsList.find(item => item.id === action.id);
+        state.addedItem.quantity--;
+        const updatedPrice =
+          state.addedItem.price * (state.addedItem.quantity - 1);
+        let newTotal = state.totalPrice - updatedPrice;
+        return { ...state, totalPrice: newTotal, addedItem: state.addedItem };
+      }
+    case GET_CART_STATE:
+      if (action.type === GET_CART_STATE) {
+        state.itemsList = action.items;
+        if (state.itemsList) {
+          state.totalItems = state.itemsList
+            .map(i => i.quantity)
+            .reduce((a, b) => a + b, 0);
+          state.totalPrice = state.itemsList
+            .map(i => i.price)
+            .reduce((a, b) => a + b, 0);
+        }
 
-export default cartReducer;
+        return {
+          ...state,
+          itemsList: action.items,
+          totalItems: state.totalItems
+        };
+      }
+    case DELETE_ITEM:
+      if (action.type === DELETE_ITEM) {
+        let newItems = state.itemsList.filter(item => item.id !== action.id);
+        state.totalItems = state.itemsList
+          .map(i => i.quantity)
+          .reduce((a, b) => a - b, 0);
+        return {
+          ...state,
+          itemsList: newItems,
+          totalItems: state.totalItems
+        };
+      }
+      break;
+    default:
+      return state;
+  }
+};
