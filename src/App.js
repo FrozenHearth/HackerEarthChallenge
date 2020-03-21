cf1db53817970c8a67a24eff56c6fe265f15efa0
Vishu <brs@Vishus-MacBook-Air.local>
2020-03-21 23:35:14 +0530

    Changed app.js into a functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import ItemsList from './components/itemsList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ItemsList />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <ItemsList />
+    </div>
+  );
+};
 
 export default App;
