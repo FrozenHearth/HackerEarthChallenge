16403fabbf7f1a143f6947c8ef73439ed4c1ef88
Vishu <brs@Vishus-MacBook-Air.local>
2020-03-21 23:38:23 +0530

    Removed dead code

--- src/components/priceCard.js
+++ src/components/priceCard.js
@@ -1,50 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { getCartState } from '../actions/cartActions';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import { CardActionArea } from '@material-ui/core';
 import '../styles/priceCard.css';
 
-class PriceCard extends Component {
-  render() {
-    return (
-      <Card className="price-card">
-        <CardContent>
-          <Typography color="textPrimary" gutterBottom>
-            Total
-          </Typography>
-          <Typography
-            gutterBottom
-            color="textSecondary"
-            variant="h6"
-            component="p"
-          >
-            Items ({this.props.totalItemCount}) : ${this.props.totalPrice}
-          </Typography>
-        </CardContent>
-
-        <CardActionArea
-          style={{
-            background: 'rgb(241, 241, 241)',
-            opacity: '0.9',
-            marginTop: '2.5em',
-            padding: '1em'
-          }}
+const PriceCard = props => {
+  return (
+    <Card className="price-card">
+      <CardContent>
+        <Typography color="textPrimary" gutterBottom>
+          Total
+        </Typography>
+        <Typography
+          gutterBottom
+          color="textSecondary"
+          variant="h6"
+          component="p"
         >
-          <Typography variant="body1" component="p">
-            Order Total : ${this.props.totalPrice}
-          </Typography>
-        </CardActionArea>
-      </Card>
-    );
-  }
-}
+          Items ({props.totalItemCount}) : ${props.totalPrice}
+        </Typography>
+      </CardContent>
+
+      <CardActionArea
+        style={{
+          background: 'rgb(241, 241, 241)',
+          opacity: '0.9',
+          marginTop: '2.5em',
+          padding: '1em'
+        }}
+      >
+        <Typography variant="body1" component="p">
+          Order Total : ${props.totalPrice}
+        </Typography>
+      </CardActionArea>
+    </Card>
+  );
+};
 
 const mapStateToProps = state => {
   return {
@@ -62,7 +57,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PriceCard);
+export default connect(mapStateToProps, mapDispatchToProps)(PriceCard);
